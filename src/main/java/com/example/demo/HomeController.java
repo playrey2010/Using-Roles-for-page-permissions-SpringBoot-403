f3425368a46d73cd84de212e916f1a699323b499
playrey2010 <playrey2010@gmail.com>
2019-10-13 16:56:35 -0400

    403 template

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -13,4 +13,9 @@ public class HomeController {
     public String login(){
         return "login";
     }
+
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
 }
